34a1a3d0421f4ac8b2c99d2bc24714ff2926e826
Rabanna Shuffle <rabannashuffle@gmail.com>
2021-08-22 19:47:01 +0200

    Configuration file and Autoloading

---
+++ core/Config.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Core;
+class Config{
+    private static $config = [
+        'version'              => '0.0.1', //app version
+         'root_dir'            => '/oopmvc', //app dir root
+         'default_controller'  => 'Ethuto', //controller
+         'default_layout'      => 'default', //degfault layout
+         'default_site_title'  => 'Ethutoapp', //site title
+         'db_host'             => '127.0.0.1', //use IP Address not domain
+         'db_name'             => 'oopmvc',//database name
+         'db_user'             => 'root',//DB user
+         'db_password'         => '', //DB password
+         'login_cookie_name'   => 'rohkjatb198619942019' //login cookie name
+
+    ];
+
+    public static function get($key){
+        return array_key_exists($key, self::$config)? self::$config[$key] : null;
+    }
+}
